1ce7ab820b279039bad6f56a7e74b34993fc2923
hee-chan123 <sk9y9y@gmail.com>
2023-09-18 12:33:14 +0900

    test 프로그램 작성

--- src/main/java/org/zerock/ex3/web/MyController.java
+++ src/main/java/org/zerock/ex3/web/MyController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MyController {
 
     @GetMapping("/main")
-    public void main(){
-        log.info("---------------------------main");
+    public void main() {
+        log.info("-------------------main");
     }
 }
